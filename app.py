f659aa1dee112cd60c34f58eeae7699cf7f1ee98
Oscar Garcia <oossgl@gmail.com>
2021-08-10 20:14:12 -0500

    Added get_regions, get_country_and_language and hash_language

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import sqlite3
-from countries import get_regions
+from countries import get_regions, get_country_and_language, hash_language
 
 # BD
 con = sqlite3.connect('zinobe.db')
 
 if __name__ == '__main__':
-    print('Hi')
-    get_regions()
+    # Get regions
+    regions = get_regions()
+    print(regions)
+
+    # Get first country of each region
+    countries = []
+    for region in regions:
+        country = get_country_and_language(region)
+        countries.append(country)
+    print(countries)
+
+    # Hash languages
+    for country in countries:
+        country['language'] = hash_language(country['language'])
+
+    print(countries)
--- countries.py
+++ countries.py
@@ -1,7 +1,10 @@
 import requests
+import json
+import hashlib
 
 
 def get_regions():
+    # Call service get all countries because there is no service to get all regions
     url = "https://restcountries-v1.p.rapidapi.com/all"
 
     headers = {
@@ -11,4 +14,39 @@ def get_regions():
 
     response = requests.request("GET", url, headers=headers)
 
-    print(response.text)
+    countries = json.loads(response.text)
+    regions = {}
+
+    # Get all regions
+    for country in countries:
+        regions[country['region']] = country['region']
+
+    # Convert dict to list
+    regions_list = list(regions.keys())
+    # Remove last value because is "" empty
+    regions_list.pop(-1)
+
+    return regions_list
+
+
+def get_country_and_language(region):
+    # Call service to get countries of a region
+    url = "https://restcountries.eu/rest/v2/region/{}".format(region)
+
+    response = requests.request("GET", url)
+
+    countries = json.loads(response.text)
+    # Get name and first language of the first country of the region
+    country = {
+        'name': countries[0]['name'],
+        'language': countries[0]['languages'][0]['name']
+    }
+
+    return country
+
+
+def hash_language(language):
+    m = hashlib.sha1()
+    m.update(language.encode('utf-8'))
+
+    return m.hexdigest()
