a5c7944cc4b5aa84cfeca53146d63ee390e50763
Oscar Garcia <oossgl@gmail.com>
2021-08-10 22:54:10 -0500

    Added times and pandas funtionality

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import sqlite3
+import time
+import pandas as pd
+
 from countries import get_regions, get_country_and_language, hash_language
 
 # BD
@@ -12,12 +15,25 @@ if __name__ == '__main__':
     # Get first country of each region
     countries = []
     for region in regions:
-        country = get_country_and_language(region)
-        countries.append(country)
-    print(countries)
+        start_time = time.time()
+        country = {'region': region}
 
-    # Hash languages
-    for country in countries:
+        country_and_language = get_country_and_language(region)
+        country['name'] = country_and_language['name']
+        country['language'] = country_and_language['language']
+
+        # Hash language
         country['language'] = hash_language(country['language'])
 
-    print(countries)
+        country['time'] = time.time() - start_time
+        countries.append(country)
+
+    # Create DataFrame
+    df = pd.DataFrame(countries)
+
+    print(df)
+
+    df_times = df.agg({'time': ['sum', 'mean', 'min', 'max']})
+    print(df_times)
+
+
